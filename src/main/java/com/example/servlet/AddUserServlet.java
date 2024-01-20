c539ff6aaf6e6b4812559065d51f4d8e61787125
Ramaz Artmeladze <ramaz.artmeladze@outlook.com>
2024-01-20 05:36:50 +0400

    Update

--- src/main/java/com/example/servlet/AddUserServlet.java
+++ src/main/java/com/example/servlet/AddUserServlet.java
@@ -2,57 +2,25 @@ package com.example.servlet;
 
 import com.example.User;
 import com.example.Warehouse;
-
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
 import java.io.IOException;
 
-@WebServlet("/add")
+@WebServlet(name = "AddUserServlet", value = "/add")
 public class AddUserServlet extends HttpServlet {
 
+    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // Get user parameters from the request
         String firstName = request.getParameter("firstName");
         String lastName = request.getParameter("lastName");
 
-        // Create a new user
         User newUser = new User(firstName, lastName);
-
-        // Add the user to the Warehouse
         Warehouse.getInstance().addUser(newUser);
 
-        // Set the user as a request attribute
         request.setAttribute("user", newUser);
-
-        // Forward to the /add page
-        request.getRequestDispatcher("/add.jsp").forward(request, response);
-    }
-
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // Get user parameters from the request
-        String firstName = (String) request.getAttribute("firstName");
-        String lastName = (String) request.getAttribute("lastName");
-
-        // Check if required attributes are present
-        if (firstName == null || lastName == null) {
-            // Handle the case where attributes are not present (e.g., redirect to an error page)
-            // Alternatively, you may want to set default values or handle this situation based on your application logic.
-            response.sendRedirect("/error.jsp"); // Redirect to an error page
-        } else {
-            // Create a new user
-            User newUser = new User(firstName, lastName);
-
-            // Add the user to the Warehouse
-            Warehouse.getInstance().addUser(newUser);
-
-            // Set the user as a request attribute
-            request.setAttribute("user", newUser);
-
-            // Forward to the /add.jsp page
-            request.getRequestDispatcher("/add.jsp").forward(request, response);
-        }
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/add.jsp");
+        dispatcher.forward(request, response);
     }
 }
+
--- src/main/java/com/example/servlet/GetUsersServlet.java
+++ src/main/java/com/example/servlet/GetUsersServlet.java
@@ -1,23 +1,22 @@
 package com.example.servlet;
 
+import com.example.User;
 import com.example.Warehouse;
-
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.util.Set;
 
-@WebServlet("/users")
+@WebServlet(name = "GetUsersServlet", value = "/users")
 public class GetUsersServlet extends HttpServlet {
 
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // Get users from the Warehouse
-        Warehouse warehouse = Warehouse.getInstance();
-        request.setAttribute("users", warehouse.getUsers());
+        Set<User> users = Warehouse.getInstance().getUsers();
+        request.setAttribute("users", users);
 
-        // Forward to the /users page
-        request.getRequestDispatcher("/users.jsp").forward(request, response);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/users.jsp");
+        dispatcher.forward(request, response);
     }
 }
